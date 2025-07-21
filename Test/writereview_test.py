40d7cc49662b9545bf172ef84eaafc62e6b9bccc
Johanna Siagian <johannasiagian3@gmail.com>
2025-07-21 09:21:12 +0700

    main

--- Test/writereview_test.py
+++ Test/writereview_test.py
@@ -15,11 +15,26 @@ do_login(driver, "andikawowsekali123@gmail.com", "andikawowsekali123")
 
 time.sleep(3)
 
-# Scroll dan klik tombol Add to Cart produk pertama
+#Step 1
+# definiskan si product link dulu
 product_link = driver.find_element(By.XPATH, '//a[@data-product-id="1"]')
+
+#scroll dulu baru klik 
 driver.execute_script("arguments[0].scrollIntoView();", product_link)
 product_link.click()
 
 print("Berhasil melihat prodyuct (view product detail)!")
+
+#Step 2  - write review lh
+#definisikan si name_input dulu biar kebaca
+name_input = driver.find_element(By.ID, "name")
+driver.execute_script("arguments[0].scrollIntoView();", name_input)
+
+name_input.send_keys("Budi")
+driver.find_element(By.ID, "email").send_keys("budi@gmail.com")
+driver.find_element(By.ID, "review").send_keys("Bajunya bagus deh keknya")
+driver.find_element(By.ID, "button-review").click()
+
+print("Berhasil menambahkan review deh")
 
 driver.quit()
